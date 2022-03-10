e7e0bcdb260f8aa99969cd7ef488726a360df84b
Luis Miguel Serrano <luis.miguel.santos.serrano@gmail.com>
2022-03-10 11:42:31 +0000

    Update index.tsx and _app.tsx for default root page and behavior

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/global.css'
 import {AppProps} from "next/app";
 
+export default MyApp
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
