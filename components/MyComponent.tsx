eb5458c028176e5dfce17cb22479a1af5dac2487
Luis Miguel Serrano <luis.miguel.santos.serrano@gmail.com>
2022-03-16 13:23:04 +0000

    Add className attribute to MyComponent

--- components/MyComponent.tsx
+++ components/MyComponent.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
-import React from "react";
+import React, {ComponentProps, HTMLAttributes, PropsWithChildren} from "react";
 
 export default MyComponent
 
 // All these functions are JSX.Element components
 //
 import {JSX} from "@babel/types"; // This is the type import which is necessary for explicit type declarations
-function MyComponent({ href, children, ...props }: {
+function MyComponent({ href, children, className, props }: {
   href: string,
   children?: React.ReactNode,
+  className?: string,
   props?: any[]
 }): JSX.Element {
   return (
     <Link href={href}>
-      <a {...props}>
+      <a className={className} {...props}>
         {children}
       </a>
     </Link>
