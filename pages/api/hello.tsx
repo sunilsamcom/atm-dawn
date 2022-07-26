bf75047376f8e4f30cdd3be7f1cf71b8ee1dcf3b
lalekidman <lalekidman@gmail.com>
2022-07-26 17:03:21 +0800

    initial implementation of logger service

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import {logger} from '@app/services/logger'
 export default function handler(req, res) {
+  try {
+    console.group("start group log")
+    console.log("Hello world")
+    console.groupEnd()
+    logger.info("JSON: ", JSON.stringify({hello: "world"}))
+    throw new Error("some error on some error.")
+  } catch (error) {
+    logger.error(error.stack)
+  }
   res.status(200).json({ name: 'John Doe' })
 }
