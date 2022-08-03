95d411f28f7cc978a4fb2279ecb862245d429275
David Santos <code@dsantos.us>
2022-08-04 00:05:48 +0100

    #2 nextauth voluum provider.

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,17 +1,13 @@
 import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
+
 import GoogleProvider from 'next-auth/providers/google'
 //import EmailProvider from 'next-auth/providers/email'
 import CredentialsProvider from "next-auth/providers/credentials";
+import { AuthData, Login } from '../../../services/voluum/login';
 
 export default NextAuth({
   providers: [
-    // OAuth authentication providers...
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
+
     // // Passwordless / email magic links sign in
     // EmailProvider({
     //     server: process.env.MAIL_SERVER,
@@ -19,25 +15,32 @@ export default NextAuth({
     // }),
     CredentialsProvider({
       // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
+      name: "Voluum",
+      id: "voluum",
       // The credentials is used to generate a suitable form on the sign in page.
       // You can specify whatever fields you are expecting to be submitted.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: {  label: "Password", type: "password" }
+        email: { label: "email", type: "email", value: process.env.VOLUUM_USERNAME || "email@rise.io" },
+        password: { label: "Password", type: "password" }
+        //  mfa: { label: "Multifactor Token", type: "text", style:"display:none" }
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const user = { id: 1, name: "J Smith", email: "jsmith@example.com" }
 
-        // const res = await fetch("/your/endpoint", {
-        //     method: 'POST',
-        //     body: JSON.stringify(credentials),
-        //     headers: { "Content-Type": "application/json" }
-        // })
-        // const user = await res.json()
+        let authData;
+        if (process.env.NODE_ENV == "development") {
+          authData = new AuthData(process.env.VOLUUM_USERNAME, process.env.VOLUUM_PASSWORD);
+          console.log("Using (.env) dev login");
+        } else {
+          authData = new AuthData(credentials.email, credentials.password);
+        }
+
+        let voluumLogin = new Login(fetch);
+        let user = await voluumLogin.login(authData);
+
+        //console.log(authData);
 
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
@@ -48,20 +51,36 @@ export default NextAuth({
 
           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
+
+        //global catch thats trhow an error and log it
       }
     })
   ],
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, user, account, profile, isNewUser }) {
+
       // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
+      if (user) {
+        
+        token.accessToken = user.authToken.token; //with acessToken we can query
+        let token_expire_ts = (new Date(user.authToken.expirationTimestamp)).getTime();
+        token.tokenExpires = parseInt(token_expire_ts/1000);
+        token.email = user.profile.email;
+        token.sub = user.profile.id;
+        token.name = user.profile.firstName + " " + user.profile.lastName;
       }
       return token
     },
     async session({ session, token, user }) {
       // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken
+      
+      let now = parseInt((new Date()).getTime()/1000);
+      if (now > token.tokenExpires){
+        return {};
+      }
+      session.accessToken = token.accessToken;
+      session.expires = (new Date(token.tokenExpires*1000))
+
       return session
     }
   }
