d4244e8e894d50df208b9e88e2bfe1b9fe843572
Luis Miguel Serrano <luis.miguel.santos.serrano@gmail.com>
2022-08-12 13:28:10 +0100

    Finetune cypress configurations and tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   videoCompression: 32,
-  video: false,
-  screenshotOnRunFailure: false,
+  video: true,
+  screenshotOnRunFailure: true,
+  videoUploadOnPasses: false,
+  trashAssetsBeforeRuns: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
