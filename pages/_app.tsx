b6e3e5e2cd536df1ad12ce68cec0416b08458586
lalekidman <lalekidman@gmail.com>
2022-08-12 15:38:02 +0800

    just reveting some unnecessary changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import '@app/styles/global.css'
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 
 import { SessionProvider } from "next-auth/react"
 export default MyApp
 
 function MyApp({ Component, pageProps }: AppProps) {
   //return <Component {...pageProps} />
-  return <>
+  return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps}/>
     </SessionProvider>
-  </>
+  )
 
 }
