31f7b646859d95a4e0796164c084fd0a669a640c
lalekidman <lalekidman@gmail.com>
2022-08-12 15:35:21 +0800

    - fixed the issue on import module alias. - implemented a basic session mocking to make the testing work.

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,7 @@ import NavigationDashboard from "@app/components/tailwindui/NavigationDashboard"
 export default Home
 
 function Home() {
-  const { data: session } = useSession() //const session = useSession().data
-  console.log("session: ", session)
+  const { data: session } = useSession() || {} //const session = useSession().data
   if(session) {
     return <>
       <h1 className="text-3xl font-bold underline">
@@ -28,69 +27,4 @@ function Home() {
     Not signed in <br/>
     <button onClick={() => signIn()}>Sign in</button>
   </>
-
-  // return (
-  //   <div className={styles.container}>
-  //     <Head>
-  //       <title>ATM</title>
-  //       <meta name="description" content="The Affiliate Tracking and Advertisement Management and Optimization platform" />
-  //       <link rel="icon" href="/favicon.ico" />
-  //     </Head>
-  //
-  //     <main className={styles.main}>
-  //       <h1 className={styles.title}>
-  //         Welcome to ATM Webapp by <a href="https://rise.io">Rise.io</a>
-  //       </h1>
-  //
-  //       <p className={styles.description}>
-  //         Get started by editing{' '}
-  //         <code className={styles.code}>pages/index.js</code>
-  //       </p>
-  //
-  //       <div className={styles.grid}>
-  //         <a href="https://nextjs.org/docs" className={styles.card}>
-  //           <h2>Documentation &rarr;</h2>
-  //           <p>Find in-depth information about Next.js features and API.</p>
-  //         </a>
-  //
-  //         <a href="https://nextjs.org/learn" className={styles.card}>
-  //           <h2>Learn &rarr;</h2>
-  //           <p>Learn about Next.js in an interactive course with quizzes!</p>
-  //         </a>
-  //
-  //         <a
-  //             href="https://github.com/vercel/next.js/tree/canary/examples"
-  //             className={styles.card}
-  //         >
-  //           <h2>Examples &rarr;</h2>
-  //           <p>Discover and deploy boilerplate example Next.js projects.</p>
-  //         </a>
-  //
-  //         <a
-  //             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-  //             className={styles.card}
-  //         >
-  //           <h2>Deploy &rarr;</h2>
-  //           <p>
-  //             Instantly deploy your Next.js site to a public URL with Vercel.
-  //           </p>
-  //         </a>
-  //       </div>
-  //     </main>
-  //
-  //     <footer className={styles.footer}>
-  //       <a
-  //           href="https://vercel.com"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //       >
-  //         Powered by{' '}
-  //         <span className={styles.logo}>
-  //           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-  //         </span>
-  //       </a>
-  //     </footer>
-  //
-  //   </div>
-  // )
 }
