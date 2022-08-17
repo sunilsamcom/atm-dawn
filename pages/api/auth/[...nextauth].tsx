a1a893eeeb4bf38d9e3db6938cf32cf7ac7dc652
lalekidman <lalekidman@gmail.com>
2022-08-17 19:48:15 +0800

    disabled the Google Provider and Custom Credential Provider.

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -9,10 +9,10 @@ import CredentialsProvider from "next-auth/providers/credentials";
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET
+    // }),
     AWSCognitoProvider({
       clientId: process.env.AWS_COGNITO_APP_CLIENT_ID,
       clientSecret: process.env.AWS_COGNITO_APP_CLIENT_SECRET,
@@ -23,41 +23,41 @@ export default NextAuth({
     //     server: process.env.MAIL_SERVER,
     //     from: 'NextAuth.js <no-reply@example.com>'
     // }),
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: {  label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-        console.log("credentials :  " , credentials)
-        console.log("req :  " , req)
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: 1, name: "J Smith", email: "jsmith@example.com" }
+    // CredentialsProvider({
+    //   // The name to display on the sign in form (e.g. "Sign in with...")
+    //   name: "Credentials",
+    //   // The credentials is used to generate a suitable form on the sign in page.
+    //   // You can specify whatever fields you are expecting to be submitted.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   // You can pass any HTML attribute to the <input> tag through the object.
+    //   credentials: {
+    //     username: { label: "Username", type: "text", placeholder: "jsmith" },
+    //     password: {  label: "Password", type: "password" }
+    //   },
+    //   async authorize(credentials, req) {
+    //     console.log("credentials :  " , credentials)
+    //     console.log("req :  " , req)
+    //     // Add logic here to look up the user from the credentials supplied
+    //     const user = { id: 1, name: "J Smith", email: "jsmith@example.com" }
 
-        // const res = await fetch("/your/endpoint", {
-        //     method: 'POST',
-        //     body: JSON.stringify(credentials),
-        //     headers: { "Content-Type": "application/json" }
-        // })
-        // const user = await res.json()
+    //     // const res = await fetch("/your/endpoint", {
+    //     //     method: 'POST',
+    //     //     body: JSON.stringify(credentials),
+    //     //     headers: { "Content-Type": "application/json" }
+    //     // })
+    //     // const user = await res.json()
 
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null
+    //     if (user) {
+    //       // Any object returned will be saved in `user` property of the JWT
+    //       return user
+    //     } else {
+    //       // If you return null then an error will be displayed advising the user to check their details.
+    //       return null
 
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      }
-    })
+    //       // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+    //     }
+    //   }
+    // })
   ],
   callbacks: {
     async jwt({ token, account }) {
