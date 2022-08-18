8e7b521180ed18f1ce8dd231592916e105fe94f7
Luis Miguel Serrano <luis.miguel.santos.serrano@gmail.com>
2022-08-18 23:41:59 +0100

    Organize winston usage and disable file logging by default because of intended serverless context

--- services/logger.ts
+++ services/logger.ts
@@ -1,5 +1,6 @@
-import { createLogger, format, transports, level as Level } from 'winston';
+import { createLogger, format, transports, level } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
+import TransportStream from "winston-transport";
 const filename = 'application'
 
 const myFormat = format.combine(
@@ -15,27 +16,27 @@ const myFormat = format.combine(
   )
 )
 
+const transportsFile: TransportStream[] = [
+  new DailyRotateFile({
+    filename: `logs/${filename}-%DATE%.log`,
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '30d',
+  }),
+  new transports.File({ filename: 'logs/error.log', level: 'error' }),
+  new transports.File({ filename: 'logs/combined.log' }),
+]
+
+const transportConsole: TransportStream = new transports.Console()
+
 export const logger = createLogger({
   level: 'info',
   format: myFormat,
   defaultMeta: { service: 'atm-dawn' },
   transports: [
-    new DailyRotateFile({
-      filename: `logs/${filename}-%DATE%.log`,
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '30d',
-    }),
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' }),
-    // - Write all logs with importance level of 'silly' or less to the console
-    new transports.Console({ level: 'silly' })
-  ],
+    transportConsole
+  ], //.concat(transportsFile),
 });
 
 // logger.configure overwrites any previous logger settings set in createLogger
@@ -43,12 +44,6 @@ export const logger = createLogger({
 //   level: `verbose`
 // })
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 if (process.env.NODE_ENV !== 'production') {
-  // logger.add(new transports.Console({
-  //   format: myFormat,
-  // }));
+  transportConsole.level = 'silly'
 }
