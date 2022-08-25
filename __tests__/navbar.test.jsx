ee4467d5b160eed49778a2f980cb1992ef6def1b
jaypoojarascalacoders <jayp@scalacoders.com>
2022-08-25 15:56:03 +0530

    commit

--- __tests__/navbar.test.jsx
+++ __tests__/navbar.test.jsx
@@ -40,7 +40,6 @@ const rightMenuItem = [
     name: "Profile",
     icon: (
       <Image
-        priority={true}
         className="h-8 w-8 rounded-full cursor-pointer"
         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
         alt="Workflow"
