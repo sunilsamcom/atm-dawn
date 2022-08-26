bab0e97f4b25fbf27385991e7e44cefb9e1254ca
jaypoojarascalacoders <jayp@scalacoders.com>
2022-08-26 19:04:51 +0530

    minor change

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,6 @@ function Home() {
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
         Signed in as {session.user.email} <br />
         Access Token: {session.access_token} <br />
-        <div>Access Token: {session.access_token}</div>
         <p></p>
         <button onClick={() => console.log("You clicked index page button!")}>
           Click me to log something!
