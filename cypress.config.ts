7de1b39878945157d85e2eba3676a93a60889cc3
sunilsamcom <sunil.k@samcomtechnobrains.com>
2022-09-02 18:41:25 +0530

    testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   videoUploadOnPasses: false,
   trashAssetsBeforeRuns: true,
   e2e: {
-    baseUrl: 'http://localhost:3000'
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
   },
 })
