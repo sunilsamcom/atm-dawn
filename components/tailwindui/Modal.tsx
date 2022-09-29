e9ddb03d762d97fabebe13eddfe76db6fa09dba7
jaldhisamcom1 <jaldhisamcom@gmail.com>
2022-09-29 19:01:19 +0530

    latest changes

--- components/tailwindui/Modal.tsx
+++ components/tailwindui/Modal.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
-import { Button, Dialog, Text as Title, Group, Alert } from "@mantine/core";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
+import {
+  Button,
+  Modal,
+  Text as Title,
+  Group,
+} from "@mantine/core";
 
 export type ModalAction = {
   label: string | React.ReactNode;
@@ -36,16 +39,15 @@ const ModalComponent: React.FC<ModalProps> = ({
   }, [props]);
 
   return (
-    <Dialog opened={open} {...props}>
-      <header className="flex align-middle justify-between">
-        {title && <Title className="font-bold pt-3 pb-2">{title}</Title>}
-        {!disableClose && (
-          <IconButton onClick={onClose}>
-            <CloseIcon />
-          </IconButton>
-        )}
-      </header>
-      {children && <Group>{children}</Group>}
+    <Modal
+      opened={open}
+      onClose={onClose}
+      withCloseButton={disableClose}
+      title={<Title className="font-bold">{title}</Title>}
+      size="50%"
+      {...props}
+    >
+      {children && children}
       {isFooterBtn && actions && (
         <Group className="flex justify-between">
           {actions.map(
@@ -63,7 +65,7 @@ const ModalComponent: React.FC<ModalProps> = ({
                 <Button
                   key={label?.toString()}
                   onClick={onClick}
-                  className="modal-action-button bg-slate-100"
+                  className="modal-action-button bg-slate-100 text-gray-500"
                   {...rest}
                 >
                   {label}
@@ -72,7 +74,7 @@ const ModalComponent: React.FC<ModalProps> = ({
           )}
         </Group>
       )}
-    </Dialog>
+    </Modal>
   );
 };
 
--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,9 +1,35 @@
 describe('Navigation Session', () => {
   it('should navigate to the login page', () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/pages/page-1');
 
-    cy.get('h1').contains('Please login:')
+    // cy.get('h1').contains('Please login:');
+
+    // cy.get('#__next');
+    // .contains("Not signed in")
+    // cy.get('#signIn').click();
+    // .contains("Sign in")
+
+    // cy.get('#input-email-for-voluum-provider').type('jaldhisamcom@gmail.com');
+    // cy.get('#input-password-for-voluum-provider').type('Samcom@84');
+    // cy.get('form').contains('form', 'Sign in with Voluum').submit();
+
+    // cy.get('.hello');
+    // cy.get('.signed_user');
+    // cy.get('.token');
+    cy.get('nav');
+    cy.get('.nav_links');
+    cy.contains('Dashboard').click();
+    // cy.contains("Campaigns and source Report").click()
+    cy.contains('Page 2').click();
+    cy.contains('Page 3').click();
+    cy.contains('Page 4').click();
+    cy.contains('Page 5').click();
+    cy.contains('Reports').click();
+    // cy.get('.notification').click();
+    // cy.get('.setting').click();
+    cy.get('.icon').click();
+    cy.contains('Your Profile').click();
 
     // // Reference default test
     // // Find a link with an href attribute containing "docs" and click it
